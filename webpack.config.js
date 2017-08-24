4f95dd299b42fa8adf8018e3c4c18d0849cf16ad
Olesya Penkova <penkoleska@gmail.com>
2017-08-24 18:49:47 +0700

    added styles

--- webpack.config.js
+++ webpack.config.js
@@ -12,20 +12,34 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "sass-loader"
+                    }],
+                    fallback: 'style-loader'
                 })
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [
-                    'file-loader'
+                    {
+                        loader: 'file-loader',
+                        options:{
+                            name: 'images/[name].[ext]'
+                        }
+                    }
                 ]
             },
            {
              test: /\.(woff|woff2|eot|ttf|otf)$/,
              use: [
-               'file-loader'
+                 {
+                     loader: 'file-loader',
+                     options:{
+                         name: 'fonts/[name].[ext]'
+                     }
+                 }
              ]
            }
         ]
