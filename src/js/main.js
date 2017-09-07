64aa08592ee823fcf7d1575bf5585e46d7bdda7d
Olesya Penkova <penkoleska@gmail.com>
2017-09-07 21:26:05 +0700

    added js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 (function() {
-    var product=$(".product");
+    var product=$(".js-product");
 
     product.on("click", function () {
-        var _this=$(this);
+        var _this=$(this).parent();
         if(!_this.data('disabled') == '1'){
             if(!_this.hasClass('product--selected')) {
                 _this.addClass('not-hover');
@@ -15,7 +15,16 @@
 
     product.on("mouseleave",function () {
         var _this=$(this);
-        _this.removeClass('not-hover');
+        _this.parent().removeClass('not-hover');
+    });
+
+    var buy=$(".js-buy");
+    buy.on("click",function (e) {
+       e.preventDefault();
+       var _this=$(this).closest('.product');
+       _this.find('.js-product').click();
+       _this.removeClass('not-hover');
+       console.log($(this).closest('.product'));
     });
 
 }());
