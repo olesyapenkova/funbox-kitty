f1bcf40c3069d4dec4caafb57bca4063f47bf206
Olesya Penkova <penkoleska@gmail.com>
2017-09-09 21:36:38 +0700

    changed index.html structure, fixed styles and js

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,34 @@
-(function() {
-    var product=$(".js-product");
+(function () {
+  var product = $(".js-product");
 
-    product.on("click", function () {
-        var _this=$(this).parent();
-        if(!_this.data('disabled') == '1'){
-            if(!_this.hasClass('product--selected')) {
-                _this.addClass('not-hover');
-            }else{
-                _this.removeClass('not-hover');
-            }
-            _this.toggleClass('product--selected');
-        }
-    });
+  /*выбираем продукт*/
+  product.on("click", function () {
+    var _this = $(this);
+    if (_this.hasClass('product--disabled'))/* проверяем а не disabled ли он*/
+      return;
 
-    product.on("mouseleave",function () {
-        var _this=$(this);
-        _this.parent().removeClass('not-hover');
-    });
+    if (!_this.hasClass('product--selected')) { /*запрещаем ховер если продукт только что выбрали, и разрешаем ховер если продукт не выбран*/
+      _this.addClass('not-hover');
+    } else {
+      _this.removeClass('not-hover');
+    }
+    _this.toggleClass('product--selected');
+    _this.next().toggleClass('footer-promo--selected');
 
-    var buy=$(".js-buy");
-    buy.on("click",function (e) {
-       e.preventDefault();
-       var _this=$(this).closest('.product');
-       _this.find('.js-product').click();
-       _this.removeClass('not-hover');
-       console.log($(this).closest('.product'));
-    });
+  });
+
+  /*разрешаем ховер на выделенном продукте*/
+  product.on("mouseleave", function () {
+    var _this = $(this);
+    _this.removeClass('not-hover');
+  });
+
+  /*выбираем продукт при клике на ссылку 'купи'*/
+  var buy = $(".js-buy");
+  buy.on("click", function (e) {
+    e.preventDefault();
+    var _this = $(this).closest('.products__item');
+    _this.find('.js-product').click().removeClass('not-hover');
+  });
 
 }());
